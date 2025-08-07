8736d4683606009b4a1fcb45072dd8b9a8d73f4f
Tomas Guzman <219607150+Eltomiass@users.noreply.github.com>
2025-08-06 23:54:45 -0600

    Actualizar app.js
    Intento de código de memoria(realizado mientras trabajo)

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let Amigos = []
+
+function fjfjfjf(){
+    let panita = document.getElementById(amigo.value);
+    if(amigos.includes(panita){
+        alert("Nombre repetido"):
+    }else {
+        Amigos.push(panita);
+        }
+    
+    
+}
